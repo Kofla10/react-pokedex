a08aeb7df2ca1707c819d6a7a7fb48eb827565bb
desarrollowc <desarrollo1@whitecloud.com.co>
2024-02-15 18:03:37 -0500

    get pokemon list

--- src/components/PokeList.jsx
+++ src/components/PokeList.jsx
@@ -1,31 +1,55 @@
 import React, {useState, useEffect} from 'react'
+import PokemonCard from './PokemonCard';
+
+
 
 export const PokeList = () => {
 
   const [allPokemons, setAllPokemons] = useState([]);
-
+  
   const getAllPokemons = async () =>{
-    const resp = await fetch("https://pokeapi.co/api/v2/pokemon?limit=649&offset=0");
+
+    const resp = await fetch("https://pokeapi.co/api/v2/pokemon?limit=10&offset=0");
 
     const data = await resp.json();
+    
+    const createPokemonObject = async (results) =>{
+      
+      results.forEach(async (pokemon) => {
 
-    function createPokemonObject(results){
-      results.array.forEach(async (pokemon) => {
-        console.log('this is a test')
         const res = await fetch(
-          `https://pokeapi.co/api/v2/pokemon${ pokemon.name }`
+          `https://pokeapi.co/api/v2/pokemon/${ pokemon.name }`
         )
 
         const data = await res.json()
 
         setAllPokemons((currentLis) => [...currentLis, data] )
+        allPokemons.sort((a,b) => a.id-b.id) //forma de organizar un arreglo
+        
       });
     }
+
+    createPokemonObject(data.results)
+    
   }
 
-  console.log(allPokemons)
+  useEffect(() => {
+    getAllPokemons()
+  }, [])  
 
   return (
-    <div>PokeList</div>
+    <>
+      <div className="app-container">
+        <div className="pokemon-container">
+          <div className="all-container">
+          {
+            allPokemons.map((pokemonData) =>(
+              <PokemonCard key={ pokemonData.id } id={ pokemonData.id.toString() }/>             
+            ))
+          }
+          </div>
+        </div>
+      </div>
+    </>
   )
 }
---
+++ src/components/PokemonCard.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+
+
+const PokemonCard = () => {
+  return (
+    <>
+        <h1>this is a test</h1>
+    </>
+  )
+}
+
+export default PokemonCard
